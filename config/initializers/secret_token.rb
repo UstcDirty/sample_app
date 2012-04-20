8c2a618db2bed26174d737d3aa8717d72469f951
UstcDirty <wang.rusticwolf@gmail.com>
2012-04-20 18:40:48 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'd07c700be9c79dcb9909274943e3cb498f24c0b47afaac18185c6d1a60ec60443abb5e24bb443c624883b65f6c533749766f86c5e08e2cb1eafd2a816460a50d'
